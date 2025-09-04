350ae3c196ccef0f26518a2c5c448ac4097cf8a8
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-09-03 21:27:19 -0600

    feat (backend): add CORS configuration

---
+++ backend_uptask/src/config/cors.ts
@@ -0,0 +1,4 @@
+import {CorsOptions} from "cors";
+export const corsConfig: CorsOptions = {
+    origin: "http://localhost:5173",
+}
--- backend_uptask/src/server.ts
+++ backend_uptask/src/server.ts
@@ -3,9 +3,14 @@ const app = express();
 import {conexionDB} from "./config/db";
 import ProyectoRouter from "./routers/ProyectoRouter";
 import TareaRouter from "./routers/TareaRouter";
+import cors from "cors";
+import {corsConfig} from "./config/cors";
 
 app.use(express.json());
 conexionDB();
+
+//Configuracion de cors
+app.use(cors(corsConfig));
 
 //Rutas de aplicacion
 app.use("/proyectos", ProyectoRouter);
